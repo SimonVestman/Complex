7f4498f3da7c83a8764614fd8e53c0d587ae73a5
SimonVestman <simonw202@gmail.com>
2020-02-06 11:15:29 +0100

    Started add

--- src/Complex.java
+++ src/Complex.java
@@ -1,3 +1,5 @@
+import javax.swing.*;
+
 /*
 Komplex datatyp
 skapat 2020-02-06
@@ -11,9 +13,15 @@ public class Complex {
         this.im = Im;
     }
 
+    public Complex add (Complex b){
+        double NewRe =
+        double NewIm =
+        return new Complex ();
+    }
     public static void main (String[]args){         //complex test
         Complex a = new Complex(3,7);
         Complex b = new Complex(5,3);
         Complex c = a.add(b);
+        System.out.println(c);
     }
 }
