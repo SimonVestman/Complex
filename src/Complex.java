c76722f0022b5ef49b9110b54a7d2e9dcd7d501e
SimonVestman <simonw202@gmail.com>
2020-02-07 11:16:39 +0100

    Fixed Add and sub and started on multiply

--- src/Complex.java
+++ src/Complex.java
@@ -1,27 +1,47 @@
-import javax.swing.*;
-
 /*
-Komplex datatyp
-skapat 2020-02-06
+Complex data type
+Created 2020-02-06
 */
+
 public class Complex {
     private double re;
     private double im;
-
+        //Constructor
     public Complex (double Re, double Im) {
         this.re = Re;
         this.im = Im;
     }
-
+        //Addition
     public Complex add (Complex b){
-        double NewRe =
-        double NewIm =
-        return new Complex ();
+        return new Complex (this.re + b.re, this.im + b.im);
     }
-    public static void main (String[]args){         //complex test
+
+        //Subtraction
+    public Complex sub (Complex b){
+        return new Complex(this.re - b.re, this.im - b.im);
+    }
+
+        //Multiplication
+    public Complex multiply (Complex b){
+        return new Complex(this.re * b.re - (this.im * b.im), this.im * b.re + (this.re * b.im));
+    }
+
+    @Override
+    public String toString() {
+        return "Complex{" + re + "+" + im + "i" + "}";
+    }
+
+    //complex test program
+    public static void main (String[]args){
         Complex a = new Complex(3,7);
         Complex b = new Complex(5,3);
         Complex c = a.add(b);
         System.out.println(c);
+
+        Complex d = a.sub(b);
+        System.out.println(d);
+
+        Complex e = a.multiply(b);
+        System.out.println(e);
     }
 }
